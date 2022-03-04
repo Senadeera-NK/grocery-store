493a83fc34767fc4b435d14832d225d196b68e3f
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-03-04 17:37:40 +0530

    2022/3/4
    created header, footer html pages roughly.

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$pagename = "Test Page";
+$pagename = "Grocery Store";
 echo "<title>".$pagename."</title>";
 echo "<h2>".$pagename."</h2>";
 echo "<hr>Success I can see the PHP page !!!";
+echo include('header.html');
+
+echo include('footer.html');
 ?>
