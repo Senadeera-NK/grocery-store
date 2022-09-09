3fa941dc970cd8b9794fd77ccb1a7c4b85dd4228
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-09 16:56:32 +0530

    2022/9/9 - 11

--- searched.php
+++ searched.php
@@ -39,8 +39,8 @@ include("header.php");
   $sql_item_names = "select `name` from ".$user_option;
   $sql_all_item_names = "select `name` from the_health_beauty_department union select `name` from the_meat_department union select `name` from the_prepared_department union select `name` from the_produce_department union select `name` from the_seafood_department union select `name` from the_wine_beer_department";
 
-  // $exeSQL_item_names = mysqli_query($connection, $sql_item_names) or die(mysqli_error($connection));
-  // $exeSQL_all_item_names = mysqli_query($connection, $sql_all_item_names) or die(mysqli_error($connection));
+  $exeSQL_item_names = mysqli_query($connection, $sql_item_names) or die(mysqli_error($connection));
+  $exeSQL_all_item_names = mysqli_query($connection, $sql_all_item_names) or die(mysqli_error($connection));
 
   // $exeSQL_search;
   // $exeSQL_option;
