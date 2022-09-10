e68d10c7b00118c949d2ebdf4504b62881246ec7
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-10 16:18:15 +0530

    2022/9/10

--- searched.php
+++ searched.php
@@ -36,86 +36,99 @@ include("header.php");
   $exeSQL_wine_beer = mysqli_query($connection, $sql_wine_beer) or die(mysqli_error($connection));
   $exeSQL_health_beauty = mysqli_query($connection, $sql_health_beauty) or die(mysqli_error($connection));
 
-  $sql_item_names = "select `name` from ".$user_option;
+
   $sql_all_item_names = "select `name` from the_health_beauty_department union select `name` from the_meat_department union select `name` from the_prepared_department union select `name` from the_produce_department union select `name` from the_seafood_department union select `name` from the_wine_beer_department";
-
-  $exeSQL_item_names = mysqli_query($connection, $sql_item_names) or die(mysqli_error($connection));
-
-
-  // $exeSQL_search;
-  // $exeSQL_option;
-  // if ($user_option == 'All'){
-  //   $exeSQL_option = "";
-  //   $exeSQL_search = $sql_all_item_names;
-  // }
-  // else if($user_option == "The Beer & Wine Department"){
-  //   $exeSQL_option = $exeSQL_wine_beer;
-  //   $exeSQL_search = $sql_item_names;
-  // }
-  // else if($user_option == "The Produce Department"){
-  //   $exeSQL_option = $exeSQL_produce;
-  //   $exeSQL_search = $sql_item_names;
-  // }
-  // else if($user_option == "The Meat Department"){
-  //   $exeSQL_option = $exeSQL_meat;
-  //   $exeSQL_search = $sql_item_names;
-  // } 
-  // else if($user_option == "The Seafood Department"){
-  //   $exeSQL_option = $exeSQL_seafood;
-  //   $exeSQL_search = $sql_item_names;
-  // } 
-  // else if($user_option == "The Health & Beauty Department"){
-  //   $exeSQL_option = $exeSQL_health_beauty;
-  //   $exeSQL_search = $sql_item_names;
-  // }
-  // else if($user_option == "The Prepared Food Department"){
-  //   $exeSQL_option = $exeSQL_prepared;
-  //   $exeSQL_search = $sql_item_names;
-  // }
+  $sql_meat_names = "select `name` from the_meat_department";
+  $sql_seafood_names = "select `name` from the_seafood_department";
+  $sql_produce_names = "select `name` from the_produce_department";
+  $sql_health_beauty_names = "select `name` from the_health_beauty_department";
+  $sql_wine_beer_names = "select `name` from the_wine_beer_department";
+  $sql_prepared_names = "select `name` from the_prepared_department";
 
 
-  // while($array_search = mysqli_fetch_array($exeSQL_search))
-  // {
-  //   $search_name = $array_search['name'];
-  //   if ($search_item == $search_name)
-  //   {
-  //     echo "wwww";
-  //   }
-  // }
+  $exeSQL_all_item_names = mysqli_query($connection, $sql_all_item_names) or die(mysqli_error($connection));
+  $exeSQL_meat_names = mysqli_query($connection, $sql_meat_names) or die(mysqli_error($connection));
+  $exeSQL_seafood_names = mysqli_query($connection, $sql_seafood_names) or die(mysqli_error($connection));
+  $exeSQL_produce_names = mysqli_query($connection, $sql_produce_names) or die(mysqli_error($connection));
+  $exeSQL_health_beauty_names = mysqli_query($connection, $sql_health_beauty_names) or die(mysqli_error($connection)); 
+  $exeSQL_wine_beer_names = mysqli_query($connection, $sql_wine_beer_names) or die(mysqli_error($connection));
+  $exeSQL_prepared_names = mysqli_query($connection, $sql_prepared_names) or die(mysqli_error($connection));
 
-  //   // ---------- 6th slider begins-----------//
-  //   echo "<h3 class='home-topics'>".$user_option."</h3>";
-  //   echo "<div class = 'gallery-wrapper'>";
-  //   echo "<img src='images/arrow.jpg' class='left' id='left6' alt='left'>";
-  //   echo "<div class='gallery' id='gallery6'>";
 
-  //   // getting all the items from mentioned sql
+  $exeSQL_search;
+  $exeSQL_option;
+  if ($user_option == 'All'){
+    $exeSQL_option = "";
+    $exeSQL_search = $exeSQL_all_item_names;
+  }
+  else if($user_option == "The Beer & Wine Department"){
+    $exeSQL_option = $exeSQL_wine_beer;
+    $exeSQL_search = $exeSQL_wine_beer_names;
+  }
+  else if($user_option == "The Produce Department"){
+    $exeSQL_option = $exeSQL_produce;
+    $exeSQL_search = $exeSQL_produce_names;
+  }
+  else if($user_option == "The Meat Department"){
+    $exeSQL_option = $exeSQL_meat;
+    $exeSQL_search = $exeSQL_meat_names;
+  } 
+  else if($user_option == "The Seafood Department"){
+    $exeSQL_option = $exeSQL_seafood;
+    $exeSQL_search = $exeSQL_seafood_names;
+  } 
+  else if($user_option == "The Health & Beauty Department"){
+    $exeSQL_option = $exeSQL_health_beauty;
+    $exeSQL_search = $exeSQL_health_beauty_names;
+  }
+  else if($user_option == "The Prepared Food Department"){
+    $exeSQL_option = $exeSQL_prepared;
+    $exeSQL_search = $exeSQL_prepared_names;
+  }
+
+
+  while($array_search = mysqli_fetch_array($exeSQL_search))
+  {
+    $search_name = $array_search['name'];
+    if ($search_item == $search_name)
+    {
+      echo "wwww";
+    }
+  }
+
+    // ---------- 6th slider begins-----------//
+    echo "<h3 class='home-topics'>".$user_option."</h3>";
+    echo "<div class = 'gallery-wrapper'>";
+    echo "<img src='images/arrow.jpg' class='left' id='left6' alt='left'>";
+    echo "<div class='gallery' id='gallery6'>";
+
+    // getting all the items from mentioned sql
   
-  //   while ($array_option = mysqli_fetch_array($exeSQL_option))
-  //   { 
-  //     //dcalring array's needed data to variables
-  //     $item_id = $array_option['id'];
-  //     $item_name = $array_option['name'];
-  //     $item_src = $array_option['path'];
-  //     $item_desc = $array_option['desc'];
-  //     $item_measurement = $array_option['measurement'];
-  //     $item_size = $array_option['size'];
-  //     $item_price = $array_option['price'];
+    while ($array_option = mysqli_fetch_array($exeSQL_option))
+    { 
+      //dcalring array's needed data to variables
+      $item_id = $array_option['id'];
+      $item_name = $array_option['name'];
+      $item_src = $array_option['path'];
+      $item_desc = $array_option['desc'];
+      $item_measurement = $array_option['measurement'];
+      $item_size = $array_option['size'];
+      $item_price = $array_option['price'];
   
-  //     //adding the js onclick function with those declared variable to output those as needed in the modal
-  //     echo "<div class='gallery-item' onclick='onClick(this, \"$item_src\", \"$item_name\", \"$item_desc\", \"$item_size\", \"$item_price\", \"$item_measurement\")' >";
+      //adding the js onclick function with those declared variable to output those as needed in the modal
+      echo "<div class='gallery-item' onclick='onClick(this, \"$item_src\", \"$item_name\", \"$item_desc\", \"$item_size\", \"$item_price\", \"$item_measurement\")' >";
     
-  //          echo "<img class='gallery-item-img' src =".$array_option['path'].">";
-  //          echo "<p class='gallery-item-name'>".$array_option['name']."</p>";
-  //          echo "<p class='gallery-item-size-rice'>".$array_option['size']." "." = "." ".$array_option['price']."</p>";
-  //     echo "</div>";
-  //   }
-  //   echo "</div>";
-  //   // right side arrow of the slider
-  //   echo "<img src='images/arrow.jpg' class='right' id ='right6' alt='right'>";
-  //   echo "</div>";
-  //   // ---------- 6th slider ends-----------//
-  //   echo "</section>";
+           echo "<img class='gallery-item-img' src =".$array_option['path'].">";
+           echo "<p class='gallery-item-name'>".$array_option['name']."</p>";
+           echo "<p class='gallery-item-size-rice'>".$array_option['size']." "." = "." ".$array_option['price']."</p>";
+      echo "</div>";
+    }
+    echo "</div>";
+    // right side arrow of the slider
+    echo "<img src='images/arrow.jpg' class='right' id ='right6' alt='right'>";
+    echo "</div>";
+    // ---------- 6th slider ends-----------//
+    echo "</section>";
   
     //------The Modal for item-----------// 
   echo "<div id='modal' class='modal'>";
